42cbaad374617bd384d3f0552ae756a06eed1378
yogi2025-as <yogeeshwara.as@gmail.com>
2025-11-18 18:06:38 +0530

    Update main.tf by Yogi

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.13.5" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "Yogiinfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.micro"
 }
